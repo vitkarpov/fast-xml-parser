2e28c6ed7d6eabbcd8bea406163f58f1fbfd6e4a
vitkarpov <viktor.karpov@toptal.com>
2019-02-25 19:18:11 +0300

    Update test names

--- e2e/parse.test.js
+++ e2e/parse.test.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
+const path = require('path');
 const {parse} = require('../dst/index');
 
 describe('parse', () => {
   fs.readdirSync('./e2e/inputs/').map((file) => {
-    return require(`./inputs/${file}`)
-  }).forEach((input) => {
-    it(input, () => {
+    return [path.basename(file, '.js'), require(`./inputs/${file}`)]
+  }).forEach(([file, input]) => {
+    it(file, () => {
       const root = parse(input);
 
       expect(root).toMatchSnapshot();
