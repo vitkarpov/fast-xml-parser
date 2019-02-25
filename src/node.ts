f3dd4863098268fdc82e8c61e35616db146779c4
vitkarpov <viktor.karpov@toptal.com>
2019-02-25 18:54:27 +0300

    Add Node interface

--- src/node.ts
+++ src/node.ts
@@ -1,1 +1,24 @@
-export class Node {}
+enum TYPES {
+  UNKNOWN,
+  OPENING_TAG,
+  CLOSING_TAG,
+  TEXT
+}
+
+interface NodeConfig {
+  type: TYPES;
+  name: string;
+  children?: Node[];
+}
+
+export class Node {
+  type: TYPES;
+  name: string;
+  children: Node[];
+
+  constructor(config: Partial<NodeConfig> = {}) {
+    this.children = config.children || [];
+    this.name = config.name || '';
+    this.type = config.type || TYPES.UNKNOWN;
+  }
+}
