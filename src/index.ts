e86a8c9aa554921b8df47529827483d2c5238d9e
vitkarpov <viktor.karpov@toptal.com>
2019-02-26 16:53:45 +0300

    Implement parser

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,28 @@
-import {Node} from './node';
+import {ok} from 'assert';
+
+import {Node, TYPES} from './node';
+import {Tokenizer} from './tokenizer';
 
 export function parse(input: string): Node {
-  return new Node();
+  const tokenizer = new Tokenizer(input);
+  const root = tokenizer.next();
+  const stack = [root];
+  let next = root;
+
+  while (next.type !== TYPES.UNKNOWN) {
+    next = tokenizer.next();
+
+    if (next.type === TYPES.OPENING_TAG) {
+      stack[stack.length - 1].children.push(next);
+      stack.push(next);
+    } else if (next.type === TYPES.TEXT) {
+      stack[stack.length - 1].children.push(next);
+    } else if (next.type === TYPES.CLOSING_TAG) {
+      stack.pop();
+    }
+  }
+
+  ok(stack.length === 0, 'The tree is malformed');
+
+  return root;
 }
